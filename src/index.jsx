d17c087c18f95c9881c80e7a54ec36ef0fd6aa93
Rcontre360 <rcontreraspimentel@gmail.com>
2021-12-07 15:38:48 -0400

    Fixed errors on functions added funcitons to submit contract interaction

--- src/index.jsx
+++ src/index.jsx
@@ -1,25 +1,24 @@
-import React, {Suspense, lazy} from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
-import {BrowserRouter} from 'react-router-dom';
-import store from './redux/';
-import * as serviceWorker from './serviceWorker';
-import Config from './configure';
-import Spinner from './components/spinner/Spinner';
+import React, { Suspense, lazy } from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import store from "./redux/";
+import * as serviceWorker from "./serviceWorker";
+import Config from "./configure";
+import Spinner from "./components/spinner/Spinner";
 import "./assets/fonts/feather/css/feather.css";
 import "./assets/fonts/fontawesome/scss/font-awesome.scss";
 import "./assets/fonts/material-design-iconic-font/css/material-design-iconic-font.min.css";
-import 'react-owl-carousel2/src/owl.carousel.css';
-import 'react-animated-slider/build/horizontal.css';
-import 'normalize.css/normalize.css';
+import "react-owl-carousel2/src/owl.carousel.css";
+import "react-animated-slider/build/horizontal.css";
+import "normalize.css/normalize.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './assets/css/animate.css';
-import './assets/css/style.css';
-import './assets/css/responsive.css';
+import "./assets/css/animate.css";
+import "./assets/css/style.css";
+import "./assets/css/responsive.css";
+import "./utility/web3";
 
-import * as web3 from './utility/web3'
-
-const App = lazy(() => import("./App/App"))
+const App = lazy(() => import("./App/App"));
 
 const root = document.getElementById("root");
 
@@ -33,7 +32,6 @@ ReactDOM.render(
   </Provider>,
   root
 );
-
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/utility/web3/index.js
+++ src/utility/web3/index.js
@@ -1,15 +1,15 @@
-import {ethers} from "ethers";
+import Web3 from "web3";
 import detectEthereumProvider from "@metamask/detect-provider";
 import abis from "../../contracts";
 import addresses from "./addresses.json";
 
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner()
-const lendingPool = new ethers.Contract(addresses.lendingPool, abis.LendingPool.abi, signer);
+const web3 = new Web3(window.ethereum);
+const lendingPool = new web3.eth.Contract(abis.LendingPool.abi, addresses.lendingPool);
+const assetToken = new web3.eth.Contract(abis.AssetToken.abi, addresses.stablecoin);
 
 //connects metamask
 export const connectMetamask = async () => {
-  await window.ethereum.request({method: "eth_requestAccounts"});
+  await window.ethereum.request({ method: "eth_requestAccounts" });
 };
 
 // ask to install metamask if returns null
@@ -21,16 +21,58 @@ export const isWeb3Enabled = async () => {
 
 // deposit x amount of stablecoin to pool
 export const depositOnLendingPool = async (amount) => {
-  const assetToken = new ethers.Contract(addresses.stablecoin, abis.AssetToken.abi, signer);
-  await assetToken.approve(lendingPool.address, amount);
-  await lendingPool.deposit(amount);
+  const accounts = await window.ethereum.request({ method: "eth_accounts" });
+  await assetToken.methods.approve(lendingPool.address, amount).send({ from: accounts[0] });
+  await lendingPool.methods.deposit(amount).send({ from: accounts[0] });
 };
 
 //change metamask to rsk network
 export const changeWalletAddress = async () => {
   await window.ethereum.request({
     method: "wallet_switchEthereumChain",
-    params: [{chainId: '0x1F'}],
+    params: [{ chainId: "0x1F" }],
   });
 };
 
+//uint256 amount,
+//uint256 interest,
+//uint8 installmentNumber,
+//uint8 installmentAmount,
+//string recipient
+//borrower must provide data(registerBorrower) to createLoan
+export const createLoan = async ({
+  amount,
+  interest,
+  installmentAmount,
+  installmentNumber,
+  recipient,
+}) => {
+  const accounts = await window.ethereum.request({ method: "eth_accounts" });
+  await lendingPool.methods
+    .createLoan(amount, interest, installmentAmount, installmentNumber, recipient)
+    .send({ from: accounts[0] });
+};
+
+//string borrowerAddress,
+//uint256 age,
+//LaboralStatus status,
+//uint256 income,
+//uint256 activity,
+//uint256 score,
+//bool evidence
+export const registerBorrower = async ({
+  borrowerAddress,
+  age,
+  status,
+  income,
+  activity,
+  score,
+  evidence,
+}) => {
+  const accounts = await window.ethereum.request({ method: "eth_accounts" });
+  await lendingPool.methods
+    .registerBorrower(borrowerAddress, age, status, income, activity, score, evidence)
+    .send({
+      from: accounts[0],
+    });
+};
