4425b1ec2336d7e9a145aeb1c24a23cb7e4f667c
Mauricio <mauriciotrejo.013@gmail.com>
2021-12-07 14:25:53 -0600

    🚧  Register project screen

--- src/view/RegisterProjectPage.jsx
+++ src/view/RegisterProjectPage.jsx
@@ -3,13 +3,16 @@ import {Container} from "reactstrap";
 import {Separator} from "../components/separator/separator";
 
 const RegisterProjectPage = () => {
-  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
   const [description, setDescription] = useState("");
   const [projectName, setProjectName] = useState("");
+  const [start, setStart] = useState("");
+  const [term, setTerm] = useState("");
+  const [file, setFile] = useState("");
 
   const changeHandler = (e) => {
-    if (e.target.name === "name") {
-      setName(e.target.value);
+    if (e.target.name === "amount") {
+      setAmount(e.target.value);
     }
     if (e.target.name === "description") {
       setDescription(e.target.value);
@@ -17,12 +20,21 @@ const RegisterProjectPage = () => {
     if (e.target.name === "projectName") {
       setProjectName(e.target.value);
     }
+    if (e.target.name === "start") {
+      setStart(e.target.value);
+    }
+    if (e.target.name === "term") {
+      setTerm(e.target.value);
+    }
+    if (e.target.name === "file") {
+      setFile(e.target.value);
+    }
   };
 
   return (
     <Fragment>
       <Container>
-        <div className="main-container profile-container">
+        <div className="main-container ">
           <h4>New Project</h4>
           <Separator />
           <section>
@@ -35,6 +47,7 @@ const RegisterProjectPage = () => {
                 <input
                   type="text"
                   id="projectName"
+                  required
                   className="register-input"
                   value={projectName}
                   name="projectName"
@@ -51,26 +64,72 @@ const RegisterProjectPage = () => {
                   id="description"
                   className="register-input"
                   value={description}
+                  required
                   rows={4}
                   name="description"
                   onChange={changeHandler}
                 />
               </div>
               <div className="form-group">
+                <label for="start" className="register-label">
+                  Start date:
+                </label>
+                <br />
+                <input
+                  type="date"
+                  id="start"
+                  placeholder="Start date"
+                  name="start"
+                  required
+                  onChange={changeHandler}
+                  value={start}
+                />
+              </div>
+              <div className="form-group">
                 <label htmlFor="name" className="register-label">
-                  Full Name
+                  Amount:
                 </label>
                 <br />
                 <input
                   type="text"
-                  id="name"
+                  id="amount"
                   className="register-input"
-                  value={name}
-                  name="name"
+                  value={amount}
+                  required
+                  name="amount"
                   onChange={changeHandler}
                 />
               </div>
-              <button type="submit" className="btn1" >Submit</button>
+              <div className="form-group">
+                <label htmlFor="name" className="register-label">
+                  Term
+                </label>
+                <br />
+                <input
+                  type="text"
+                  id="term"
+                  required
+                  className="register-input"
+                  value={term}
+                  name="term"
+                  onChange={changeHandler}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="name" className="register-label">
+                  Attachments
+                </label>
+                <br />
+                <input
+                  type="file"
+                  name="file"
+                  className="register-input"
+                  onChange={changeHandler}
+                />
+              </div>
+              <button type="submit" className="btn1" style={{marginTop: 20}}>
+                Submit
+              </button>
             </form>
           </section>
         </div>
